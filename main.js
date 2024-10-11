1acf6af42693617ebe435bdc39ace7d88b6d7b8d
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-10-11 13:54:40 +0200

    Small change for better readablility

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import Board from './board.js';
 
 const board = new Board();
-const nightMoves = board.knightMoves([0, 0], [7, 7]);
+const startSquare = [0, 0];
+const endSquare = [7, 7];
+const nightMoves = board.knightMoves(startSquare, endSquare);
 console.log(nightMoves);
